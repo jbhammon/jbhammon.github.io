6eb9baf7ad6572fdd73ccb797fbded1db88270fa
Jackson Hammond <jbhammon@users.noreply.github.com>
2019-01-31 18:33:13 -0500

    Fixed underline issues on small screens

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,5 @@
 $("#heroTextFirst").delay(1000).animate({opacity: 1, left: "+=5"}, 700);
 
 $("#heroTextSecond").delay(1500).animate({opacity: 1, left: "+=5"}, 700);
-$("#heroUnderline").delay(1800).animate({width: '100%'}, 300, 'swing');
 
 $("#heroTextThird").delay(2000).animate({opacity: 1, left: "+=5"}, 700);
