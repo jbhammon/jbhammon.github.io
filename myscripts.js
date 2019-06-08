a631849c5cefeb3d04d0579d366a32e20de192be
Jackson Hammond <jbhammon@users.noreply.github.com>
2019-06-08 00:11:18 -0400

    Replaced the Bootstrap navbar with my own that uses my own media queries

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,14 @@
-$("#heroTextFirst").delay(500).animate({opacity: 1, left: "+=10"}, 700);
+// $("#heroTextFirst").delay(500).animate({opacity: 1, left: "+=10"}, 700);
+//
+// $("#heroTextSecond").delay(1500).animate({opacity: 1, left: "+=5"}, 700);
+//
+// $("#heroTextThird").delay(2000).animate({opacity: 1, left: "+=5"}, 700);
 
-$("#heroTextSecond").delay(1500).animate({opacity: 1, left: "+=5"}, 700);
-
-$("#heroTextThird").delay(2000).animate({opacity: 1, left: "+=5"}, 700);
+function navBarHandling() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "myNavBar") {
+    x.className += " responsive";
+  } else {
+    x.className = "myNavBar";
+  }
+}
