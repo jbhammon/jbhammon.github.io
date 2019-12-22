ce171731a863b653751cec3088a1b7f7b2439db5
Jackson Hammond <jbhammon@users.noreply.github.com>
2019-12-22 16:52:15 -0500

    Initial homescreen thumbnails added

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,14 +1,8 @@
-// $("#heroTextFirst").delay(500).animate({opacity: 1, left: "+=10"}, 700);
-//
-// $("#heroTextSecond").delay(1500).animate({opacity: 1, left: "+=5"}, 700);
-//
-// $("#heroTextThird").delay(2000).animate({opacity: 1, left: "+=5"}, 700);
-
 function navBarHandling() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "myNavBar") {
+  if (x.className === "topnav") {
     x.className += " responsive";
   } else {
-    x.className = "myNavBar";
+    x.className = "topnav";
   }
 }
