15a52db320723335653b851a77bf7285ec593ff1
Jackson Hammond <jbhammon@users.noreply.github.com>
2020-02-08 17:33:13 -0500

    Added button to mapme project

--- scripts/backToTop.js
+++ scripts/backToTop.js
@@ -11,13 +11,6 @@ function scrollFunction() {
 }
 
 function goBackToTop() {
-  // let position = document.body.scrollTop || document.documentElement.scrollTop;
-  //   if (position) {
-  //       window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-  //       scrollAnimation = setTimeout("goBackToTop()", 10);
-  //   } else {
-  //     clearTimeout(scrollAnimation);
-  //   }
   document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 }
