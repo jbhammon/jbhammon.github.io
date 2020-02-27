ff6c9f3a57e8adeb711f0c0f654c61f62e251ff1
Jackson Hammond <jbhammon@users.noreply.github.com>
2020-02-27 17:33:12 -0500

    Finished first draft of goodlistens page

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -10,8 +10,14 @@ function navBarHandling() {
 function openDesign(event) {
   document.getElementById('design-work').style.display = "flex";
   document.getElementById('dev-work').style.display = "none";
+
+  document.getElementById('design-tab').classList.add('active-tab');
+  document.getElementById('dev-tab').classList.remove('active-tab');
 }
 function openDev(event) {
   document.getElementById('design-work').style.display = "none";
   document.getElementById('dev-work').style.display = "flex";
+
+  document.getElementById('design-tab').classList.remove('active-tab');
+  document.getElementById('dev-tab').classList.add('active-tab');
 }
